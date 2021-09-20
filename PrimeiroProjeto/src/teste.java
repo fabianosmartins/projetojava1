959f0294be1c037990e59fe7a9b4fd6c53be7a07
Fabiano M <Fabiano M@DESKTOP-R89IUIV>
2021-09-20 19:23:45 -0300

    Criação do Projeto

---
+++ PrimeiroProjeto/src/teste.java
@@ -0,0 +1,20 @@
+import java.util.Date;
+public class teste {
+
+	public static void main(String[] args) {
+
+	
+		Date date = new Date();
+		
+		//date.getYear()
+		
+		
+		
+		System.out.println("Hoje é dia " + date.getDate() + " de " +(date.getYear() + 1900));
+		
+		//System.out.println(date.getDate());
+					
+
+	}
+
+}
